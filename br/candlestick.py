dc2f893b5a682a1ea8415052b8b7159d6355a0f9
javadebadi <javad.ebadi.1990@gmail.com>
2021-09-11 00:29:06 +0430

    Updated BR

--- br/candlestick.py
+++ br/candlestick.py
@@ -18,18 +18,18 @@ class CandleStickBR:
         self.candlestick_ts = None
 
     def get_raw_df(self):
-        logging.info(f"CandleStick: started to get raw OHLC data for {self.symbol} at {self.interval}")
+        # logging.info(f"CandleStick: started to get raw OHLC data for {self.symbol} at {self.interval}")
         da = CandleStickDA()
         df = da.get_raw_df(self.symbol, self.interval)
         self.candlestick_ts = CandlestickTimesSeries(df)
-        logging.info(f"CandleStick: created a candlestick times series for OHLC raw data ")
+        # logging.info(f"CandleStick: created a candlestick times series for OHLC raw data ")
 
     def add_features(self):
         self.candlestick_ts.add_candlestick_features()
-        logging.info(f"CandleStick: Added candlestick features")
+        # logging.info(f"CandleStick: Added candlestick features")
 
     def insert_or_update_candlestick_table(self):
-        logging.info(f"CandleStick: created a CandleStickDMList object")
+        # logging.info(f"CandleStick: created a CandleStickDMList object")
         candlestick_dm_list_obj = CandleStickDMList()
         candlestick_dm_list_obj.reset()
         for value in self.candlestick_ts.to_values():
@@ -38,8 +38,8 @@ class CandleStickBR:
             candlestick_dm_list_obj.append(candlestick_dm_obj)
 
         da = CandleStickDA()
-        logging.warning(f'CandleStick: Started to fill candlestick table')
+        # logging.warning(f'CandleStick: Started to fill candlestick table')
         da.insert_or_update_if_exists(candlestick_dm_list_obj.to_list_of_values())
-        logging.warning(f'CandleStick: Finished filling of candlestick table in database')
+        # logging.warning(f'CandleStick: Finished filling of candlestick table in database')
 
     
