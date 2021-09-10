94c2a91be7fbc5e02bc444dcbb9661d6891008d4
javadebadi <javad.ebadi.1990@gmail.com>
2021-09-11 00:30:20 +0430

    Added main

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from datetime import (
     timedelta,
 )
 from multiprocessing import Process
-
+from quanttools.strategy import RegressionSupportResistanceStrategy
+import time
     
 import logging
-logging.basicConfig(filename='myapp.log', level=logging.INFO, format='%(asctime)s %(message)s')
+logging.basicConfig(filename='myapp.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 MINUTE = 60
 HOUR = 60*MINUTE
@@ -35,18 +36,45 @@ def _run_symbols_job():
         symbol_br.insert_or_update_symbol_table()
         time.sleep(DAY)
 
+
+
+
+def run_regression_strategy():
+    lr_strategy = RegressionSupportResistanceStrategy(symbol='FTM-USDT', interval='30min')
+    lr_strategy.set_apply_unix_time(unix_time=time.time())
+    lr_strategy.load_historical_data()
+    lr_strategy.set_strategy_hyperparameters(
+        support_train_data_size=15,
+        resistance_train_data_size=15,
+        min_kind='min_o1',
+        max_kind='max_o1'
+        )
+    # print(lr_strategy)
+    lr_strategy.take_positions()
+
 def fill_candlestick():
     # add candlestick features to candlestick table
-    # for TRX-USDT
-    candlestick_br = CandleStickBR('TRX-USDT', '30min')
-    candlestick_br.get_raw_df()
-    candlestick_br.add_features()
-    candlestick_br.insert_or_update_candlestick_table()
-    # for VRA-USDT
-    candlestick_br = CandleStickBR('VRA-USDT', '30min')
-    candlestick_br.get_raw_df()
-    candlestick_br.add_features()
-    candlestick_br.insert_or_update_candlestick_table()
+    # for all symbols in watch list
+    from data_access.da import SymbolDA
+    da = SymbolDA()
+    watch_list = [item[0] for item in da.get_watch_lists()]
+    for symbol in watch_list:
+        candlestick_br = CandleStickBR(symbol, '30min')
+        candlestick_br.get_raw_df()
+        candlestick_br.add_features()
+        candlestick_br.insert_or_update_candlestick_table()
+
+
+
+def _run_onetime_ohlc_job():
+    logging.warning('Started One time OHLC reading job from API ...')  # will print a message to the console
+    ohlc_br = OHLCBR()
+    ohlc_br.insert_or_update_ohlc_table(
+        interval='30min',
+        start=int(time.time()-365*DAY),
+        end=int(time.time()))
+    fill_candlestick()
+    logging.warning("Finished One time job of getting all past months data from API ...")
 
 def _run_ohlc_job():
     logging.warning('Started OHLC reading job from API ...')  # will print a message to the console
@@ -55,11 +83,11 @@ def _run_ohlc_job():
             continue
         ohlc_br = OHLCBR()
         ohlc_br.insert_or_update_ohlc_table(
-            interval='30min',
-            start=int(time.time()-365*DAY),
-            end=int(time.time()))
+            interval='30min')
         fill_candlestick()
-        time.sleep(HOUR)
+        # take positions
+        run_regression_strategy()
+        time.sleep(3*MINUTE)
 
         
 def _run_django():
@@ -68,6 +96,8 @@ def _run_django():
 def run():
     """a fucntion to run all """
     logging.info('Started')
+    # run onetime job first
+    _run_onetime_ohlc_job()
     # run django server
     p_django = Process(target=_run_django)
     p_django.start()
@@ -87,6 +117,6 @@ def run():
 
 
 if __name__ == '__main__':
-    # run()
+    run()
     # _run_ohlc_job()
-    fill_candlestick()
+    # fill_candlestick()
